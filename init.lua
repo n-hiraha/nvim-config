8f17df217674e6421ffedcf7d512d02fad2c45d5
n-hirahara <genenao21@gmail.com>
2022-11-09 11:46:42 +0900

    [add] null-ls

--- init.lua
+++ init.lua
@@ -114,3 +114,21 @@ mason.setup({} )
 lspconfig.setup { 
   ensure_installed = { "sumneko_lua", "tailwindcss" }, 
 }
+
+-- null_ls and prettier
+local null_ls = require "null-ls"
+null_ls.setup {
+  sources = {
+    null_ls.builtins.formatting.deno_fmt.with {
+      condition = function(utils)
+        return not (utils.has_file { ".prettierrc", ".prettierrc.js", "deno.json", "deno.jsonc" })
+      end,
+    },
+    null_ls.builtins.formatting.prettier.with {
+      condition = function(utils)
+        return utils.has_file { ".prettierrc", ".prettierrc.js" }
+      end,
+      prefer_local = "node_modules/.bin",
+    },
+  },
+}
